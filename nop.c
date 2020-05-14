cbc5de1d5a8baefe876b0d50fd549e5679dcd63a
Taha Yazidi <yazi.taha@gmail.com>
2020-05-14 19:57:45 +0000

    nop_opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing.
+ * @stack: pointer to the top of stack.
+ * @line_n: line number.
+ */
+void nop(stack_t **stack, unsigned int line_n)
+{
+	(void)stack;
+	(void)line_n;
+}
