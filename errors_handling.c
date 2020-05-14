44491d32819fc81eb61d7161f43180aeb16c96d5
Taha Yazidi <yazi.taha@gmail.com>
2020-05-14 18:43:42 +0000

    error_handling

--- errors_handling.c
+++ errors_handling.c
@@ -38,3 +38,40 @@ void errors(int error_code, ...)
 	free_node();
 	exit(EXIT_FAILURE);
 }
+
+/*
+ * errors1 - more errors.
+ * @error_code: error code.
+ */
+void errors1(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+	case 6:
+		fprintf(stderr, "L%d: can't pint, stack empty\n",
+		       va_arg(ag, int));
+		break;
+	case 7:
+		fprintf(stderr, "L%d: can't pop an empty stack\n",
+		       va_arg(ag, int));
+		break;
+	case 8:
+		l_num = va_arg(ag, unsigned int);
+		op = va_arg(ag, char *);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+		break;
+	case 9:
+		fprintf(stderr, "L%d: division by zero\n",
+		       va_arg(ag, unsigned int));
+		break;
+	default:
+		break;
+	}
+	free_node();
+	exit(EXIT_FAILURE);
+}
